2a4e5553771c9d00ba1d39ee4af04b05dd8236a2
abdicore <abdirhamanmohamed@gmail.com>
2024-10-02 16:41:30 +0200

    create form-control

--- index.php
+++ index.php
@@ -14,20 +14,23 @@ $saluto = "ciao"
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <?php echo $saluto?>
     <!-- form -->
      <main class="container">
         <section class="row">
             <div class="col-12">
-                <div class="mb-3">
-                    <label for="exampleFormControlTextarea1" class="form-label">Iserisci il tuo paragrafo</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                </div>                
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Inserisci la parola da nascondere</label>
-                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-                </div>
-                </div>
+                <h1></h1>
+                <form action="./datareceived.php" method="GET">
+                    <div class="mb-3">
+                         <label for="Paragrafo" class="form-label">Iserisci il tuo paragrafo</label>
+                        <textarea class="form-control" id="Paragrafo" placeholder="Inserisci il tuo paragrafo" rows="3"></textarea>  
+                    </div>                
+                    <div class="mb-3">
+                        <label for="parola-censurata" class="form-label">Scegli una parola da nascondere</label>
+                        <input type="text" class="form-control" id="parola-censurata" placeholder="Inserisci la parola da nascondere">
+                    </div>               
+                </form>
+               
+            </div>
         </section>
      </main>
     
